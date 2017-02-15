be66d06cffff99f29b867ed017cd238897afc2df
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-02-15 23:03:29 +0530

    Create Fibonacci_Series.c

---
+++ Fibonacci_Series/Fibonacci_Series.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a=0, b=1, c=0, len;
+    printf("Enter the length of Fibonacci Series : ");
+    scanf("%d", &len);
+    len=len-2;   /*since we are printing first two elements outside the loop.*/
+    printf("%d %d ", a, b);
+    while(len){
+    c=a+b;
+    a=b;
+    b=c;
+    printf("%d ", c);
+    len--;
+    }
+    return 0;
+}
