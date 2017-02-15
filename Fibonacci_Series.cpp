e81c3c77c5e857bd329214bd3d6968024026cf33
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-02-15 22:46:53 +0530

    Create Fibonacci_Series.cpp

---
+++ Fibonacci_Series.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a=0, b=1, c=0, len;
+    cout<<"Enter the length of Fibonacci Series : ";
+    cin>>len;
+    len=len-2;  //since we are printing first two elements outside the loop.
+    cout<<a<<" "<<b<<" ";
+    while(len){
+    c=a+b;
+    a=b;
+    b=c;
+    cout<<c<<" ";
+    len--;
+    }
+return 0;
+}
